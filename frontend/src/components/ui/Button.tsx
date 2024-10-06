2d4413906963000d5b1a0ea269e8a65fbd37a8a7
devhasanmia <dev.hasanmia@gmail.com>
2024-10-06 07:52:04 +0600

    stock Adjustment

--- frontend/src/components/ui/Button.tsx
+++ frontend/src/components/ui/Button.tsx
@@ -2,14 +2,12 @@ type TButton = {
   type: "button" | "submit" | "reset";
   background: string;
   text: string;
-  onChange?: () => void;
 };
 
-const Button = ({ type, background, text, onChange }: TButton) => {
+const Button = ({ type, background, text }: TButton) => {
   return (
     <button
       type={type}
-      onChange={onChange}
       className={`w-full bg-${background}-500  text-white py-2 px-4  rounded-lg shadow-md hover:bg-${background}-600 transition duration-300`}
     >
       {text}
--- frontend/src/pages/Stock/StockAdjustment.tsx
+++ frontend/src/pages/Stock/StockAdjustment.tsx
@@ -1,9 +1,8 @@
 import { SubmitHandler, useForm } from "react-hook-form";
-import Input from "../../components/ui/input";
 import Label from "../../components/ui/Label";
 import { useGetAllProductQuery } from "../../redux/api/api";
-import { useState } from "react";
 import Button from "../../components/ui/Button";
+
 type Inputs = {
   product: string;
   quantity: number;
@@ -16,83 +15,49 @@ type Inputs = {
   due: number;
   extraExpense: number;
 };
+
+type TProduct = {
+  _id: string;
+  productName: string;
+};
+
 const StockAdjustment = () => {
   const { data } = useGetAllProductQuery(undefined);
+
   const { register, handleSubmit, reset } = useForm<Inputs>();
-  const [totalPrice, setTotalPrice] = useState(0);
-  const handleChange = (e) => {
-    console.log(e);
-  }
+
   const onSubmit: SubmitHandler<Inputs> = (formData) => {
-    setTotalPrice(formData.quantity * formData.salePrice);
-    formData.totalPrice = totalPrice;
     console.log(formData);
     reset();
   };
+
   return (
     <div className="min-h-screen bg-gray-100 p-6">
       <h1 className="text-3xl font-semibold text-gray-700 mb-8 text-center">
-        স্টক এডজাস্টমেন্ট করুন
+        নতুন পণ্যদ্রব্য ক্রয় করুন
       </h1>
-      <hr />
-      <br />
-      {/* Form Component */}
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="bg-white shadow-md rounded-lg p-6 max-w-lg mx-auto"
-      >
+
+      <form onSubmit={handleSubmit(onSubmit)} className="bg-white shadow-md rounded-lg p-6 max-w-screen-lg mx-auto">
         <div className="mb-4">
-          <Label name="productName" text="পণ্যের নাম" />
+          <Label name="product" text="পণ্যের নাম" />
           <select
-            className="mt-1 focus:ring-2 focus:ring-[#1677ff] block w-full border border-gray-300 rounded-md p-2 focus:outline-none"
             {...register("product")}
+            className="mt-1 focus:ring-2 focus:ring-[#1677ff] block w-full border border-gray-300 rounded-md p-2 focus:outline-none"
           >
             <option value="">পণ্য নির্বাচন করুন</option>
-            {data?.data.map((product) => (
+            {data?.data?.map((product: TProduct) => (
               <option key={product._id} value={product._id}>
                 {product.productName}
               </option>
             ))}
           </select>
         </div>
-        <div className="mb-4">
-          <Label name="quantity" text="পরিমান" />
-          <Input
-            type="number"
-            placeholder="পরিমান"
-            register={register("quantity")}
-
-          />
+        <div className="space-x-5">
+          <Label name="quantity" text="পরিমান"/>
         </div>
-        <div className="mb-4">
-          <Label name="purchasePrice" text="ক্রয় মূল্য" />
-          <Input
-            type="number"
-            placeholder="ক্রয় মূল্য লিখুন"
-            register={register("purchasePrice")}
-          />
-        </div>
-        <div className="mb-4">
-          <Label name="sellingPrice" text="বিক্রয় মূল্য" />
-          <Input
-            type="number"
-            placeholder="বিক্রয় মূল্য"
-            register={register("salePrice")}
-          />
-        </div>
-        <div className="mb-4">
-          <Label name="totalPrice" text="মোট টাকা" />
-          <input
-            {...(register("totalPrice") && { value: totalPrice })}
-            type="number"
-            placeholder="মোট টাকা"
-            value={totalPrice}
-            className="mt-1 focus:ring-2 focus:ring-[#1677ff] block w-full border border-gray-300 rounded-md p-2 focus:outline-none"
-          />
-        </div>
-        <div className="mb-4">
-          <Button type="submit" text="Submit" background="blue" />
-        </div>
+        {/* Submit Button Start */}
+        <Button type="submit" text="পণ্য ক্রয় করুন" background="red" />
+        {/* Submit Button End */}
       </form>
     </div>
   );
